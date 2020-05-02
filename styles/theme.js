8730f6b847972dbb68ba0a4aac81a25638acaa76
Paulo Henrique <pauloh1288@gmail.com>
2020-05-01 21:08:26 -0300

    chore(style): add initial theming config

---
+++ styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  palette: {
+    primaryLightest: '#F0F5FE',
+    primaryLighter: '#CEDEFD',
+    primaryLight: '#9DBCFB',
+    primaryRegular: '#4882F7',
+    primaryDark: '#1241A1',
+    primaryDarker: '#103070',
+  },
+};
