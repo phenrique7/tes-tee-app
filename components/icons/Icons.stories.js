f1b616c9a5fc5d87ed3daa669326d4682ab48f77
Paulo Henrique <pauloh1288@gmail.com>
2020-05-02 11:41:42 -0300

    docs(stories): add icons stories

---
+++ components/icons/Icons.stories.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import AddIcon from './Add';
+import CartIcon from './Cart';
+import CloseIcon from './Close';
+import EqualsIcon from './Equals';
+import FacebookIcon from './Facebook';
+import HamburgerIcon from './Hamburger';
+import InstagramIcon from './Instagram';
+import MultiplyIcon from './Multiply';
+import SearchIcon from './Search';
+import SubtractIcon from './Subtract';
+import TwitterIcon from './Twitter';
+
+export default {
+  title: 'Icons',
+};
+
+export const add = () => <AddIcon />;
+export const cart = () => <CartIcon />;
+export const close = () => <CloseIcon />;
+export const equals = () => <EqualsIcon />;
+export const hamburger = () => <HamburgerIcon />;
+export const multiply = () => <MultiplyIcon />;
+export const search = () => <SearchIcon />;
+export const subtract = () => <SubtractIcon />;
+export const instagram = () => <InstagramIcon />;
+export const facebook = () => <FacebookIcon />;
+export const twitter = () => <TwitterIcon />;
