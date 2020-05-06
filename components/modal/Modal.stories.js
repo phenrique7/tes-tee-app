de57bb497fd1f3da400803290fafa750426155a3
Paulo Henrique <pauloh1288@gmail.com>
2020-05-05 21:54:03 -0300

    refactor(storybook): improvement Modal stories

--- components/modal/Modal.stories.js
+++ components/modal/Modal.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../button/Button';
 import Modal from './Modal';
+import uuid from '../../utils/uuid';
 
 export default {
   title: 'Modal',
@@ -9,12 +10,14 @@ export default {
 
 export const standard = () => {
   const [showModal, setShowModal] = React.useState(false);
+  const productId = uuid();
 
   return (
     <>
       <Button onClick={() => setShowModal(true)}>Open modal</Button>
       <Modal
         showModal={showModal}
+        productId={productId}
         productImage="/assets/shirts/blue-tee.png"
         productName="TES-TEE blue cotton shirt"
         productSizes={['XS', 'L', 'XL']}
@@ -24,12 +27,17 @@ export const standard = () => {
   );
 };
 
-export const content = () => (
-  <Modal
-    showModal
-    productImage="/assets/shirts/blue-tee.png"
-    productName="TES-TEE blue cotton shirt"
-    productSizes={['XS', 'L', 'XL']}
-    closeModal={() => {}}
-  />
-);
+export const content = () => {
+  const productId = uuid();
+
+  return (
+    <Modal
+      showModal
+      productId={productId}
+      productImage="/assets/shirts/red-tee.png"
+      productName="TES-TEE red cotton shirt"
+      productSizes={['XS', 'XXL', 'L', 'XL']}
+      closeModal={() => {}}
+    />
+  );
+};
